d9feb08ef2eff44d92be40f1e7d0038ca8fe8fe1
Ntose <163145946+Ntose@users.noreply.github.com>
2025-04-18 12:42:06 +0200

    testing movement script

--- Task01/Source.cpp
+++ Task01/Source.cpp
@@ -1,21 +1,46 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Window/Keyboard.hpp>
 
+float step = 1;
+float x = 0, y = 0;
+sf::CircleShape shape(70.f);
+
+static void MoveVertical() {
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+		y -= step;
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+		y += step;
+}
+
+static void MoveHorizontal() {
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+		x -= step;
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+		x += step;
+}
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode({ 1000, 1000 }), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
+	while (window.isOpen())
+	{
+		while (const std::optional event = window.pollEvent())
+		{
+			if (event->is<sf::Event::Closed>())
+				window.close();
+		}
+		// Movement functions
+		MoveVertical();
+		MoveHorizontal();
+		shape.setPosition({ x, y });
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
 }
+
